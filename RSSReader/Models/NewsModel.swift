1b7c62984e5af84136b0a0fde58a160c123507f8
Anton Aliokhna <antonaliokhna@gmail.com>
2023-02-03 21:42:34 +0300

    Add news model to project

---
+++ RSSReader/Models/NewsModel.swift
@@ -0,0 +1,17 @@
+//
+//  NewsModel.swift
+//  RSSReader
+//
+//  Created by Anton Aliokhna on 2/3/23.
+//
+
+import Foundation
+
+struct NewsModel {
+    let author: String
+    let title: String
+    let description: String
+    let pubDate: String
+    let enclosure: String
+    let category: String
+}
